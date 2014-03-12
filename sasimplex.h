f0e052b434673cc79fc29cc03fc18662b6426174
Alan R. Rogers <rogers@anthro.utah.edu>
2014-03-12 15:58:06 +0000

    Plan: get rid of gsl_multimin_fminimizer_sasimplexrand. Incorporate the random rotations it provides into a new function, sasimplex_randomize_state, which also optionally chooses a random starting point. Then the user begins by creating an object of typs gsl_multimin_fminimizer_sasimplex. Whenever a new random simplex is desired, the user hands this objec to randomize_state. Before doing this, the user should seed the random number generator. Failure to do so should generate an error.

--- sasimplex.h
+++ sasimplex.h
@@ -14,5 +14,7 @@ void        sasimplex_random_seed(gsl_multimin_fminimizer * minimizer,
                                   unsigned seed);
 void        sasimplex_set_temp(gsl_multimin_fminimizer * minimizer,
                                double temperature);
+void        sasimplex_randomize_state(gsl_vector *x, gsl_vector *lo,
+                                      gsl_vector *hi, unsigned *seedPtr);
 
 #endif                       /* __GSL_SASIMPLEX_H__ */
